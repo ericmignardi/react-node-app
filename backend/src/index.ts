fc9e2789fc741bd1146919adf4ac49e3ac6eaa5e
Eric Mignardi <mignardi.e@gmail.com>
2026-06-21 07:11:03 -0400

    Setup minimal express server

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import "dotenv/config";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK", timestamp: new Date().toISOString() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
